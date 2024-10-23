cc78bb7f4e0edb43f295c61ddd854ada26b7e7c9
unknown <Admin@STUDENT13>
2024-10-23 13:44:52 +0100

    EmployeeServices

--- src/main/java/com/ims/ui/UiController.java
+++ src/main/java/com/ims/ui/UiController.java
@@ -1,5 +1,13 @@
 package com.ims.ui;
 
+/**
+ * @author TheHamer
+ * @version 23/10
+ *
+ * - Interacts with EmployeeDAO for CRUD operations.
+ * - Contains business logic such as validations, input sanitization, etc.
+ */
+
 public class UiController {
 
     public void Initialise(){
