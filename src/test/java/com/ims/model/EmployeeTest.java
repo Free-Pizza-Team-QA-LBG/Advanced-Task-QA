3b8b6356a381b76324aba220757679aa79e18883
alexp <28978970+AlexPTerry@users.noreply.github.com>
2024-10-23 14:09:34 +0100

    Fixed employee string formatting and completed Employee tests

--- src/test/java/com/ims/model/EmployeeTest.java
+++ src/test/java/com/ims/model/EmployeeTest.java
@@ -21,8 +21,26 @@ public class EmployeeTest {
     @Test
     public void constructorTest() {
         assertNotNull(testEmployee);
+        assertEquals(1, testEmployee.getId());
+        assertEquals("Test", testEmployee.getFirstName());
+        assertEquals("Man", testEmployee.getLastName());
+        assertEquals("hi@free-pizza.com", testEmployee.getEmail());
+        assertEquals("QA", testEmployee.getDepartment());
+        assertEquals(1.0f, testEmployee.getSalary(), 0.0);
     }
 
-
+    @Test
+    public void toStringTest() {
+        assertEquals("""
+                Employee details:
+                    ID: 1
+                    Name: Test Man
+                    Email: hi@free-pizza.com
+                    Department: QA
+                    Salary: 1.00
+                    """
+                , testEmployee.toString()
+        );
+    }
 
 }
