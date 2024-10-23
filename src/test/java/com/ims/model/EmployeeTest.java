2d031074aa58814d96b1436951a75a92f6604e4d
alexp <28978970+AlexPTerry@users.noreply.github.com>
2024-10-23 14:15:11 +0100

    More exhaustive employee testing

--- src/test/java/com/ims/model/EmployeeTest.java
+++ src/test/java/com/ims/model/EmployeeTest.java
@@ -21,12 +21,32 @@ public class EmployeeTest {
     @Test
     public void constructorTest() {
         assertNotNull(testEmployee);
+    }
+
+    @Test
+    public void getterTest() {
         assertEquals(1, testEmployee.getId());
         assertEquals("Test", testEmployee.getFirstName());
         assertEquals("Man", testEmployee.getLastName());
         assertEquals("hi@free-pizza.com", testEmployee.getEmail());
         assertEquals("QA", testEmployee.getDepartment());
         assertEquals(1.0f, testEmployee.getSalary(), 0.0);
+    }
+
+    @Test
+    public void setterTest() {
+        testEmployee.setId(2);
+        assertEquals(2, testEmployee.getId());
+        testEmployee.setFirstName("John");
+        assertEquals("John", testEmployee.getFirstName());
+        testEmployee.setLastName("Wick");
+        assertEquals("Wick", testEmployee.getLastName());
+        testEmployee.setEmail("hello@free-pizza.com");
+        assertEquals("hello@free-pizza.com", testEmployee.getEmail());
+        testEmployee.setDepartment("Finance");
+        assertEquals("Finance", testEmployee.getDepartment());
+        testEmployee.setSalary(12.0f);
+        assertEquals(12.0f, testEmployee.getSalary(), 0.0);
     }
 
     @Test
