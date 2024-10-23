c2bf7208f5eeaaf25aeacdf8f216a9b02bebd450
Jordan Taylor <nadroj.taylor99@gmail.com>
2024-10-23 11:01:15 +0100

    UI Controller doc comment

--- src/main/java/com/ims/ui/UiController.java
+++ src/main/java/com/ims/ui/UiController.java
@@ -1,5 +1,20 @@
 package com.ims.ui;
 
+
+
+/**
+ User Interface (UI):
+ - In the ui package, implement a simple CLI-based interface to interact with the system.
+ - It should allow the user to:
+ - Add an employee.
+ - View employee details by ID.
+ - Update employee details.
+ - Delete an employee by ID.
+ - List all employees.
+ - Use Scanner for reading input from the user
+ */
+
+
 public class UiController {
 
     public void Initialise(){
