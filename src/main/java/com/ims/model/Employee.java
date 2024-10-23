f6207178298e15a8746b971d929f584a18dfa6e2
alexp <28978970+AlexPTerry@users.noreply.github.com>
2024-10-23 11:58:00 +0100

    Created Employee class file

---
+++ src/main/java/com/ims/model/Employee.java
@@ -0,0 +1,13 @@
+package com.ims.model;
+
+/**
+ * @author Alex Terry
+ * @version 23/10/2024
+ *  - Implement the Employee class in the model package. It should have:
+ *    - Fields matching the database structure: id, first_name, last_name, email, department, salary.
+ *    - Getters and Setters for all fields.
+ *    - A constructor to initialize the Employee object.
+ *    - A toString() method for printing employee details.
+ */
+public class Employee {
+}
