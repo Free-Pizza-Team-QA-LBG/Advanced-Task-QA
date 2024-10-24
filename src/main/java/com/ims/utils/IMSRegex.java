f9db6096ccdaa3cb158fe6d73aa5000962abb859
BenSnellgrove <ben.e.snellgrove@gmail.com>
2024-10-24 13:29:30 +0100

    TDD: Morse Regex + Tests!

--- src/main/java/com/ims/utils/IMSRegex.java
+++ src/main/java/com/ims/utils/IMSRegex.java
@@ -2,10 +2,14 @@ package com.ims.utils;
 
 /**
  * @author BenSnellgrove
- * @version 23/10/2024
+ * @version 24/10/2024
  */
 public class IMSRegex {
 
     public static final String EMAIL_REGEX = "^([a-zA-Z0-9\\_\\-\\.]*)@([a-z0-9\\_\\-]*)\\.(com|(co\\.)?uk)$";
+
+
+    public static final String MORSE_REGEX = "^((([\\-\\.]){1,4}) *\\/? *)+$";
+
 
 }
--- src/test/java/com/ims/utils/IMSRegexTest.java
+++ src/test/java/com/ims/utils/IMSRegexTest.java
@@ -11,11 +11,13 @@ import static org.junit.Assert.*;
 public class IMSRegexTest {
 
     Pattern emailPattern;
+    Pattern morsePattern;
     Matcher matcher;
 
     @Before
     public void before() {
         emailPattern = Pattern.compile(IMSRegex.EMAIL_REGEX);
+        morsePattern = Pattern.compile(IMSRegex.MORSE_REGEX);
     }
 
     @Test
@@ -41,6 +43,22 @@ public class IMSRegexTest {
         assertTrue(matcher.matches());
         matcher = emailPattern.matcher("test@company-name.uk");
         assertTrue(matcher.matches());
+
+    }
+
+
+    @Test
+    public void morseMatchNormal() {
+
+        matcher = morsePattern.matcher("...          --- ..         .");
+        assertTrue(matcher.matches());
+
+        matcher = morsePattern.matcher(".--- .- ...- .- / .. ... / -.-. --- --- .-.. / --- -.-");
+        assertTrue(matcher.matches());
+
+        matcher = morsePattern.matcher(".---- .- ...- .- / .. ... / -.-. --- --- .-.. / --- -.-");
+        assertFalse(matcher.matches());
+
 
     }
 
