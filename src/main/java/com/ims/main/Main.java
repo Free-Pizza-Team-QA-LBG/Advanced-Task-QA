7d0f76e0f952e2813655eb6eeda38347c359a941
Jordan Taylor <nadroj.taylor99@gmail.com>
2024-10-24 13:36:13 +0100

    janky testing but testing barely works so help

--- src/main/java/com/ims/main/Main.java
+++ src/main/java/com/ims/main/Main.java
@@ -9,7 +9,17 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        /*
+        MorseTranslator mt = new MorseTranslator();
+        System.out.println("==============================================");
+        System.out.println( mt.translateBackwardsInnefficient("hello"));
+        System.out.println("==============================================");
 
+        System.out.println("==============================================");
+        System.out.println( mt.translate("hello"));
+        System.out.println("==============================================");
+        */
+         
 
         StaticUiController.InitialiseUI();
 
